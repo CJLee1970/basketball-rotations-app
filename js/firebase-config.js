6462e8b3afc5b529bfb69893a22b83da25f471e0
CJLee1970 <cjlee09@me.com>
2025-08-10 20:56:03 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,23 @@
-// Firebase config (client-side config is safe to expose; secure your data via Firestore Rules)
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// js/firebase-config.js
+// Using Firebase *compat* because we include the compat CDN scripts in HTML.
+// Do NOT use `import` lines here.
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDA6iCgvu_HMGHkc3JV4qJoio8CSGekcl8",
+  apiKey: "YOUR_API_KEY",
   authDomain: "basketballrotations2025.firebaseapp.com",
   projectId: "basketballrotations2025",
-  storageBucket: "basketballrotations2025.firebasestorage.app",
-  messagingSenderId: "824151000894",
-  appId: "1:824151000894:web:88c4582c2b9317868a4e6f",
-  measurementId: "G-MK4QLJQSJ1"
+  storageBucket: "basketballrotations2025.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialise (guarded so it won't double-init if run twice)
+if (!firebase.apps || !firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Expose for other scripts
+window.auth = firebase.auth();
+window.db = firebase.firestore();
+
