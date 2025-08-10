63c2be9989ed207f291daf444aaeb5830e5a8075
CJLee1970 <cjlee09@me.com>
2025-08-10 10:42:58 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,13 @@
 // Firebase config (client-side config is safe to expose; secure your data via Firestore Rules)
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCT0zZbCMotQsYzQRFtWQOI7Ew35AbXwDc",
+  apiKey: "AIzaSyDA6iCgvu_HMGHkc3JV4qJoio8CSGekcl8",
   authDomain: "basketballrotations2025.firebaseapp.com",
   projectId: "basketballrotations2025",
   storageBucket: "basketballrotations2025.firebasestorage.app",
@@ -8,6 +15,7 @@ const firebaseConfig = {
   appId: "1:824151000894:web:88c4582c2b9317868a4e6f",
   measurementId: "G-MK4QLJQSJ1"
 };
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
