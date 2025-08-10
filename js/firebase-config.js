551e7030f05bc4572c3d6feea8edf5f806b20237
CJLee1970 <cjlee09@me.com>
2025-08-10 20:58:19 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,13 +3,13 @@
 // Do NOT use `import` lines here.
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyDA6iCgvu_HMGHkc3JV4qJoio8CSGekcl8",
   authDomain: "basketballrotations2025.firebaseapp.com",
   projectId: "basketballrotations2025",
-  storageBucket: "basketballrotations2025.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  storageBucket: "basketballrotations2025.firebasestorage.app",
+  messagingSenderId: "824151000894",
+  appId: "1:824151000894:web:88c4582c2b9317868a4e6f",
+  measurementId: "G-MK4QLJQSJ1"
 };
 
 // Initialise (guarded so it won't double-init if run twice)
